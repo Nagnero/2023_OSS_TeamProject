4c43964ff24b5efc8fa6b105c63ce845f9a2b99d
Nagnero <davidyeon123@naver.com>
2023-12-06 23:12:03 +0900

    feat: data pre-processing func.

--- project/scrap.py
+++ project/scrap.py
@@ -1,31 +1,59 @@
 import csv
 import requests
 from bs4 import BeautifulSoup
-
-stock_names = []
-rates = []
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 url = "https://finance.naver.com/sise/lastsearch2.naver"
-
 filename = "1-30.csv"
-f = open(filename, "w", encoding="utf-8-sig", newline="")
-writer = csv.writer(f)
+#f = open(filename, "w", encoding="utf-8-sig", newline="")
+#writer = csv.writer(f)
 
 res = requests.get(url)
 res.raise_for_status()
 soup = BeautifulSoup(res.text, "lxml")
-#print(soup.find("table", attrs={"class":"type_5"}).find_all("tr"))
+
 data_rows = soup.find("table", attrs={"class":"type_5"}).find_all("tr")
 
-for row in data_rows:
-  # check the row has valid data
-  no_column = row.find_all("td")
-  if len(no_column) <= 1:   # 의미 없는 데이터 skip
-    continue
-  data = [column.get_text().strip() for column in no_column]
-  writer.writerow(data)
+with open(filename, "w", encoding="utf-8-sig", newline="") as file:
+  writer = csv.writer(file)
+  # save 30 stock data to csv
+  for row in data_rows:
+    # check the row has valid data
+    no_column = row.find_all("td")
+    if len(no_column) <= 1:   # 의미 없는 데이터 skip
+      continue
+    data = [column.get_text().strip() for column in no_column]
+    writer.writerow(data)
+
+# save filtered stock
+filtered_data = []
+
+with open("1-30.csv", "r", newline="", encoding="utf-8") as file:
+  reader = csv.reader(file)
+  print(reader)
+  for row in reader:
+    if row[5] and any(sign in row[5] for sign in ['+', '-']):
+      change = float(row[5].replace("+", "").replace("-", "").replace("%", "").strip())
+      if abs(change) >= 10.0:
+        filtered_data.append(row)
+
+# 필터링된 데이터를 새로운 CSV 파일에 씀
+with open("filtered_data.csv", "w", newline="", encoding="utf-8") as outfile:
+  writer = csv.writer(outfile)
+    
+  # 필터링된 데이터를 쓰기
+  writer.writerows(filtered_data)
 
 
+# browser search
+# browser = webdriver.Chrome()
+# browser.get("http://naver.com")
+
+# elem = browser.find_element_by_id("query")
+# elem.send_keys("z")
+# elem.sec_keys(Keys.ENTER)
+# browser.exit()
 
     #print(data)
   # if no_column:
