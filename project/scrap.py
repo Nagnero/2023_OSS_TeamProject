8c6545b616d74650f4c080aa30d79c3cc27cbf49
Nagnero <davidyeon123@naver.com>
2023-12-06 18:20:36 +0900

    feat: scrap data and save as csv

--- project/scrap.py
+++ project/scrap.py
@@ -1,13 +1,15 @@
 import csv
 import requests
 from bs4 import BeautifulSoup
-import certifi
 
 stock_names = []
 rates = []
 
 url = "https://finance.naver.com/sise/lastsearch2.naver"
-#headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"}
+
+filename = "1-30.csv"
+f = open(filename, "w", encoding="utf-8-sig", newline="")
+writer = csv.writer(f)
 
 res = requests.get(url)
 res.raise_for_status()
@@ -17,20 +19,31 @@ data_rows = soup.find("table", attrs={"class":"type_5"}).find_all("tr")
 
 for row in data_rows:
   # check the row has valid data
-  no_column = row.find("td", attrs={"class":"no"})
-  if no_column:
-    anchor_tag = row.find("a")
-    # append valid data to array
-    if anchor_tag:
-      # save stock name
-      stock_names.append(anchor_tag.text)
-      # save flunctuate rate
-      rate_column = row.find_all("td", attrs={"class":"number"})
-      rates.append(rate_column[3].find("span").text)
+  no_column = row.find_all("td")
+  if len(no_column) <= 1:   # 의미 없는 데이터 skip
+    continue
+  data = [column.get_text().strip() for column in no_column]
+  writer.writerow(data)
 
 
-for stock in stock_names:
-  print(stock)
 
-for rate in rates:
-  print(rate)
+    #print(data)
+  # if no_column:
+  #   anchor_tag = row.find("a")
+  #   # append valid data to array
+  #   if anchor_tag:
+  #     writer.writerow(anchor_tag)
+  #     # save stock name
+  #     stock_names.append(anchor_tag.text)
+      
+  #     # save flunctuate rate
+  #     rate_column = row.find_all("td", attrs={"class":"number"})
+  #     rates.append(rate_column[3].find("span").text.strip())
+      
+
+
+# for stock in stock_names:
+#   print(stock)
+
+# for rate in rates:
+#   print(rate)
